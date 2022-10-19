48d7db5da325c80c235a42516122aa955e6706ec
Your Name <adnaneramargho@gmail.com>
2022-10-19 22:40:49 +0600

    hexadecimal to octal done

---
+++ C/ocataltohexadecimal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    int a;
+    printf("decimal number");
+    scanf("%d", &a);
+    printf("\n Octal number %o", a);
+    printf("\n hexadecimal Number %x", a);
+
+    return 0;
+}
