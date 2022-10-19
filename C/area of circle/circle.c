08885e066fb7bd2585c1d5752691ac61a31bcab3
Your Name <adnaneramargho@gmail.com>
2022-10-19 23:32:44 +0600

    circle

---
+++ C/area of circle/circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14
+int main(int argc, char const *argv[])
+{
+    /* code */
+    float a, area;
+    printf("r");
+    scanf("%f", &a);
+    area = 2 * PI * a;
+    printf("%f", area);
+    return 0;
+}
